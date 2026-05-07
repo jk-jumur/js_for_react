94388f25e3a7e2052a6176c71c23fb67abf36f37
jk-jumur <developer.jumur@gmail.com>
2026-05-08 01:48:33 +0600

    spread operator create new object

--- 6.spread_rest_operator.js
+++ 6.spread_rest_operator.js
@@ -1,4 +1,4 @@
-//Rest 
+//Rest  Operator
 function myFunc(a, b, ...rest){
       console.log(a, b, rest);
 }
@@ -6,18 +6,42 @@ function myFunc(a, b, ...rest){
 // myFunc(1, 2, 3, 4, 6, 67, 7, 7, 100, 200);
 
 
-//spread operator
+//spread operator -> Independently copy korar jonno spread operator use kore thaki
 //new array create korte pari  
 let arr = [1, 2, 3, 4, 5];
 let arr2 = [6, 7, 8, 9, 10];
 
 //  console.log(...arr);// ekhane array ta fure number ee return korbe
 
-// let arr3 = [...arr, ...arr2];
+let arr3 = [...arr, ...arr2];
 // console.log(arr3);
 
-
-let arr3 = arr;
 arr.push(500);
+
+
+// let arr3 = arr;
+// arr.push(500);
 console.log(arr);
 console.log(arr3);
+
+
+//Object
+
+
+let person = {
+    name: "Jk",
+    age: 23,
+};
+
+let computer = {
+     monitor: "LG",
+     ssd: "256GB",
+     graphicsCard: "8GB"
+}
+
+//new object create
+let newObj = {
+     ...person, ...computer
+}
+
+console.log(newObject);
