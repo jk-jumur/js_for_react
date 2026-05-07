d9ec5b71b556809900373e3a93b5ac0780e77413
jk-jumur <developer.jumur@gmail.com>
2026-05-08 01:36:08 +0600

    spread res operator0

--- 6.spread_rest_operator.js
+++ 6.spread_rest_operator.js
@@ -3,11 +3,21 @@ function myFunc(a, b, ...rest){
       console.log(a, b, rest);
 }
 
-myFunc(1, 2, 3, 4, 6, 67, 7, 7, 100, 200);
+// myFunc(1, 2, 3, 4, 6, 67, 7, 7, 100, 200);
 
 
 //spread operator
-
+//new array create korte pari  
 let arr = [1, 2, 3, 4, 5];
 let arr2 = [6, 7, 8, 9, 10];
 
+//  console.log(...arr);// ekhane array ta fure number ee return korbe
+
+// let arr3 = [...arr, ...arr2];
+// console.log(arr3);
+
+
+let arr3 = arr;
+arr.push(500);
+console.log(arr);
+console.log(arr3);
