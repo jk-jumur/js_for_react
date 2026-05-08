a25614d470150e1e6f742022b7937a72f529186c
jk-jumur <developer.jumur@gmail.com>
2026-05-08 21:56:07 +0600

    && || operator ternary operator concept

---
+++ 9.shortcut_condition.js
@@ -0,0 +1,14 @@
+//&& , ||, Ternary, ?(optional chaining)
+
+const name = "JK";
+const age = 23;
+// const isMatured = true;
+  const isMatured = false;
+
+const test = isMatured && name;
+const test2 = isMatured || name;
+console.log(test);
+console.log(test2);
+
+//ternary operator
+
