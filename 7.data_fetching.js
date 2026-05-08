7026fc47443a9928d6efc4ca87ec283b3ebf6f10
jk-jumur <developer.jumur@gmail.com>
2026-05-08 15:34:42 +0600

    data fetching es6 formate

--- 7.data_fetching.js
+++ 7.data_fetching.js
@@ -1,8 +1,24 @@
- const fetchUsers = ()=>{
-      fetch("https://jsonplaceholder.typicode.com/users")
-     .then ((res) => res.json())
-     .then(data => console.log(data));
+//  const fetchUsers = ()=>{
+//       fetch("https://jsonplaceholder.typicode.com/users")
+//      .then ((res) => res.json())
+//      .then(data => {
+//         //   console.log(data)
+//           throw new Error("unknown error");
+//      })
+//      .catch((e) => console.log(e));
+//  }
+
+//  fetchUsers();
+ 
+ 
+ //ES6 er Formate
+
+ const fetchUsers = async () =>{
+     const res = await fetch ("https://jsonplaceholder.typicode.com/users")
+     const data = await res.json();
+     console.log(data);
+
+
  }
- 
- 
- 
+
+ fetchUsers();
