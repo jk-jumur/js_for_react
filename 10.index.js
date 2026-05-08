7fa24ee33509052807303ee6f91d13557591edbd
jk-jumur <developer.jumur@gmail.com>
2026-05-08 23:46:36 +0600

    ES6 modules import export concept

---
+++ 10.index.js
@@ -0,0 +1,9 @@
+let name = "JK";
+let age = 23;
+
+// console.log(name, age);
+export { name, age };
+
+//ekta jinish export korte chaile single er jonno default use korte hoy
+
+export default name;
---
+++ 11.index2.js
@@ -0,0 +1,5 @@
+import { name as MyName, age } from "./10.index.js";
+
+//   import Myname, { name } from "./10.index.js";
+// console.log(MyName);
+// console.log(name);
